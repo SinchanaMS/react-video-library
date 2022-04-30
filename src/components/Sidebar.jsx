f7e6f1b6e4b20a9e83f6b3f221c87a01055a3f9f
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2022-04-30 16:23:37 +0530

    feat: added header and sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,40 @@
+import {
+  MdHome,
+  MdExplore,
+  MdHistory,
+  MdPlaylistPlay,
+  MdThumbUp,
+  MdWatchLater,
+} from "react-icons/md";
+import { Link } from "react-router-dom";
+import { useTheme } from "../contexts/ThemeContext";
+import "../styles/sidebar.css";
+
+export default function Sidebar() {
+  const { theme } = useTheme();
+  return (
+    <nav className={theme === "light" ? "sidebar" : "sidebar dark"}>
+      <Link className="sidebar-links" to="/">
+        <MdHome className="sidebar-icons" />
+        Home
+      </Link>
+      <Link className="sidebar-links" to="/">
+        <MdExplore className="sidebar-icons" />
+        Explore
+      </Link>
+      <Link className="sidebar-links" to="/history">
+        <MdHistory className="sidebar-icons" /> History
+      </Link>
+      <Link className="sidebar-links" to="/playlist">
+        <MdPlaylistPlay className="sidebar-icons" /> Playlist
+      </Link>
+      <Link className="sidebar-links" to="/liked">
+        <MdThumbUp className="sidebar-icons" />
+        Liked Videos
+      </Link>
+      <Link className="sidebar-links" to="/watchlater">
+        <MdWatchLater className="sidebar-icons" /> Watch Later
+      </Link>
+    </nav>
+  );
+}
