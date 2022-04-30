7c89066d8542f5c6ea61887c3e2ba9af7dd9c01f
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2022-04-30 16:25:06 +0530

    feat: added video listing page

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,3 +1,48 @@
+import axios from "axios";
+import { useState } from "react";
+import Sidebar from "../../components/Sidebar";
+import { useTheme } from "../../contexts/ThemeContext";
+import "../../styles/videolisting.css";
+
 export default function Explore() {
-  return <div>Explore</div>;
+  const { theme } = useTheme();
+  const [videoList, setVideoList] = useState([]);
+
+  const videos = async () => {
+    const response = await axios.get("/api/videos");
+    try {
+      if (response.status === 200) {
+        setVideoList(response.data.videos);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  videos();
+  return (
+    <div className="main-content">
+      <Sidebar />
+      <div className={theme === "light" ? "video-list" : "video-list dark"}>
+        {videoList.map(
+          ({ id, thumbnail: { url }, title, creatorImg, creator, views }) => (
+            <div
+              className={theme === "light" ? "video-card" : "video-card dark"}
+              key={id}
+            >
+              <img src={url} alt={title} className="video-thumbnail" />
+              <div className="video-details">
+                <img src={creatorImg} className="creator-dp" />
+                <div className="creator-text">
+                  <h3 className="video-title">{title}</h3>
+                  <p className="video-creator p-sm p-grey">{creator}</p>
+                  <p className="video-views p-sm p-grey">{views} views</p>
+                </div>
+              </div>
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  );
 }
