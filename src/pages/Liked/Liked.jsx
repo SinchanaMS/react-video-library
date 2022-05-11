731afe954211ecdc7a023cdfd7d0059b6aca7257
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2022-05-11 18:10:20 +0530

    feat: added the like feature

--- src/pages/Liked/Liked.jsx
+++ src/pages/Liked/Liked.jsx
@@ -1,3 +1,17 @@
+import VideoCard from "components/VideoCard";
+import { useTheme, useVideo } from "contexts/contexts";
+
 export default function Liked() {
-  return <div>Liked</div>;
+  const {
+    videoData: { likedList },
+  } = useVideo();
+  const { theme } = useTheme();
+
+  return (
+    <div className={theme === "light" ? "video-list" : "video-list dark"}>
+      {likedList.map((video) => (
+        <VideoCard video={video} key={video._id} />
+      ))}
+    </div>
+  );
 }
