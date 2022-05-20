00d16a1f48ad69a4321dbe5ffb6dd9805812d9eb
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2022-05-20 22:45:49 +0530

    feat: search and sort videos

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,31 +1,46 @@
 import { useTheme, useVideo } from "contexts/contexts";
 import "styles/videolisting.css";
-import { useState } from "react";
 
 export default function CategoryList() {
-  const { videoData, allVideos, filterByCategory } = useVideo();
+  const { videoData, videoDispatch } = useVideo();
   const { theme } = useTheme();
   const { categoryList } = videoData;
-  const [activeCategory, setActiveCategory] = useState("");
-
-  const categoryHandler = (categoryName) => {
-    setActiveCategory(categoryName);
-    filterByCategory(categoryName, allVideos);
-  };
 
   return (
     <ul className={theme === "light" ? "category-list" : "category-list dark"}>
+      <li
+        className={
+          "All" === videoData.category ? "category active" : "category"
+        }
+        onClick={() => videoDispatch({ type: "SET_CATEGORY", payload: "All" })}
+      >
+        All
+      </li>
       {categoryList?.map(({ _id, categoryName }) => (
         <li
           key={_id}
           className={
-            categoryName === activeCategory ? "category active" : "category"
+            categoryName === videoData.category ? "category active" : "category"
           }
-          onClick={() => categoryHandler(categoryName)}
+          onClick={() =>
+            videoDispatch({ type: "SET_CATEGORY", payload: categoryName })
+          }
         >
           {categoryName}
         </li>
       ))}
+      <li
+        className={
+          "Latest" === videoData.sortBy ? "category active" : "category"
+        }
+        onClick={() =>
+          videoData.sortBy === "Latest"
+            ? videoDispatch({ type: "SORT_BY", payload: "" })
+            : videoDispatch({ type: "SORT_BY", payload: "Latest" })
+        }
+      >
+        Latest
+      </li>
     </ul>
   );
 }
