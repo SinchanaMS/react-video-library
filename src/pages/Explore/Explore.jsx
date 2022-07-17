76993ce882c0e564f3e8abc8fa057f1082730699
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2022-07-17 14:04:08 +0530

    fix: broken layout

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -8,21 +8,19 @@ export default function Explore() {
   const { filteredVideos, loading } = useVideo();
 
   return (
-    <div className="main-content">
-      <div className="explore-videos">
-        <CategoryList />
-        <div className="video-list">
-          {filteredVideos?.length === 0 ? (
-            <div className="empty-page">
-              <img src={empty} alt="empty-page" />
-              <p>{loading ? "Loading videos..." : "No results found."}</p>
-            </div>
-          ) : (
-            filteredVideos?.map((video) => (
-              <VideoCard video={video} key={video._id} />
-            ))
-          )}
-        </div>
+    <div className="explore-videos">
+      <CategoryList />
+      <div className="video-list">
+        {filteredVideos?.length === 0 ? (
+          <div className="empty-page">
+            <img src={empty} alt="empty-page" />
+            <p>{loading ? "Loading videos..." : "No results found."}</p>
+          </div>
+        ) : (
+          filteredVideos?.map((video) => (
+            <VideoCard video={video} key={video._id} />
+          ))
+        )}
       </div>
     </div>
   );
